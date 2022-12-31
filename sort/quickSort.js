0f5f00dcfb6e12afb2f58eba5c55f67433657003
fish <dy179912099@163.com>
2022-12-31 15:42:04 +0800

    feat:quick sort

---
+++ sort/quickSort.js
@@ -0,0 +1,29 @@
+Array.prototype.quickSort = function () {
+  const rec = (arr) => {
+    if (arr.length <= 1) {
+      return arr;
+    }
+
+    const mid = arr[0];
+
+    const left = [];
+    const right = [];
+
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] < mid) {
+        left.push(arr[i]);
+      } else {
+        right.push(arr[i]);
+      }
+    }
+    return [...rec(left), mid, ...rec(right)];
+  };
+
+  const res = rec(this);
+
+  res.forEach((n, i) => (this[i] = n));
+};
+
+const arr = [5, 4, 3, 2, 1];
+
+arr.quickSort();
